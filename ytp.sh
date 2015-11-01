4aa31053440a6302a120d92c09510c198692aa2e
Steven Maude <git@stevenmaude.co.uk>
2015-11-01 11:37:57 +0000

    Add initially working script

---
+++ ytp.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# ytp.sh <playlist_file>
+# Take a list of YouTube URLs; one per line.
+# Generates a playlist URL that doesn't need you to be logged in.
+
+# Copyright Steven Maude 2015
+# Licensed under GPLv3.
+set -eu
+
+mystring=""
+OIFS=$IFS
+IFS="="
+
+while read BASE_URL ID
+do
+  if [ "$mystring" = "" ]; then
+    mystring="$ID"
+  else
+    mystring="${mystring},${ID}"
+  fi
+done <$1
+
+IFS=$OIFS
+echo "https://www.youtube.com/watch_videos?video_ids=${mystring}"
